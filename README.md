bb565b391e556e637aa2e7dbdd6c1d60982d6d99
Julio Valsesia <jcvalsesia@avaya.com>
2024-01-24 11:51:08 -0300

    Find unique items - Begin

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-# level-up-rust
-Rust advanced 
+# Level Up Rust: Challenge 2
+
+Your challenge is to implement a function, `unique()`, that takes a vector of floating point numbers (`Vec<f32>`) and returns a list that only contains unique values.
+
+## Testing your code
+
+To test your solution, use `cargo test`.
+
+```console
+$ cargo test
+...
+````
 
 # References
 https://github.com/LinkedInLearning/level-up-rust-3041143
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,73 @@
+fn unique(a: Vec<i32>) -> Vec<i32> {
+    todo!()
+}
+
+// advanced 1: use generic types
+// fn unique(a: Vec<T>) -> Vec<T> {
+//     todo!();
+// }
+
+// advanced 2: keep items in order
+// fn unique(a: Iterable<T>) -> Vec<T> {
+//     todo!();
+// }
+
+// advanced 3: use iterators
+// fn unique(a: Iterable<T>) -> Vec<T> {
+//     todo!();
+// }
+
 fn main() {
-    println!("Hello, world!");
+    let input = vec![2, 1, 1];
+    let answer = unique(input);
+    println!("unique items -> {:?}", answer)
+}
+
+#[test]
+fn empty_list() {
+    let input = vec![];
+    let expected_output = vec![];
+    let actual_output = unique(input);
+    assert_eq!(actual_output, expected_output);
+}
+
+#[test]
+fn sorted_list() {
+    let input = vec![1, 4, 5];
+    let mut expected_output = vec![1, 4, 5];
+    let mut actual_output = unique(input);
+    expected_output.sort_unstable();
+    actual_output.sort_unstable();
+    assert_eq!(actual_output, expected_output);
+}
+
+#[test]
+fn unsorted_list() {
+    let input = vec![1, 5, 2];
+    let mut expected_output = vec![1, 2, 5];
+    let mut actual_output = unique(input);
+    expected_output.sort_unstable();
+    actual_output.sort_unstable();
+    assert_eq!(actual_output, expected_output);
+}
+
+#[test]
+fn unsorted_list_with_duplicates() {
+    let input = vec![1, 5, 2, 2, 1];
+    let mut expected_output = vec![1, 2, 5];
+    let mut actual_output = unique(input);
+    expected_output.sort_unstable();
+    actual_output.sort_unstable();
+    assert_eq!(actual_output, expected_output);
+}
+
+#[test]
+fn sorted_list_with_duplicates() {
+    let mut input = vec![1, 5, 2, 2, 1];
+    input.sort_by(|x, y| x.partial_cmp(y).unwrap());
+    let mut expected_output = vec![1, 2, 5];
+    let mut actual_output = unique(input);
+    expected_output.sort_unstable();
+    actual_output.sort_unstable();
+    assert_eq!(actual_output, expected_output);
 }
