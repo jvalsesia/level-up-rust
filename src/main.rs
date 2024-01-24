99fbc00146c440bc40fb76bd60b3a06cc6bc67c8
Julio Valsesia <jcvalsesia@avaya.com>
2024-01-24 11:52:19 -0300

    Find unique items - End

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
-fn unique(a: Vec<i32>) -> Vec<i32> {
-    todo!()
+fn unique(mut a: Vec<i32>) -> Vec<i32> {
+    a.sort();
+    a.dedup();
+    a
 }
 
-// advanced 1: use generic types
-// fn unique(a: Vec<T>) -> Vec<T> {
-//     todo!();
+// // advanced 1.2: use generic types
+// fn unique<T: Ord>(mut a: Vec<T>) -> Vec<T> {
+//     a.sort();
+//     a.dedup();
+//     a
 // }
 
-// advanced 2: keep items in order
+// // advanced 1.1: use generic types
+// fn unique<T: PartialOrd>(mut a: Vec<T>) -> Vec<T> {
+//     a.sort_by(|x,y| { x.partial_cmp(y).unwrap() });
+//     a.dedup();
+//     a
+// }
+
+// // advanced 2: keep items in order
 // fn unique(a: Iterable<T>) -> Vec<T> {
 //     todo!();
 // }
 
-// advanced 3: use iterators
+// // advanced 3: use iterators
 // fn unique(a: Iterable<T>) -> Vec<T> {
 //     todo!();
 // }
@@ -25,8 +36,8 @@ fn main() {
 
 #[test]
 fn empty_list() {
-    let input = vec![];
-    let expected_output = vec![];
+    let input: Vec<i32> = vec![];
+    let expected_output: Vec<i32> = vec![];
     let actual_output = unique(input);
     assert_eq!(actual_output, expected_output);
 }
@@ -34,30 +45,24 @@ fn empty_list() {
 #[test]
 fn sorted_list() {
     let input = vec![1, 4, 5];
-    let mut expected_output = vec![1, 4, 5];
-    let mut actual_output = unique(input);
-    expected_output.sort_unstable();
-    actual_output.sort_unstable();
+    let expected_output = vec![1, 4, 5];
+    let actual_output = unique(input);
     assert_eq!(actual_output, expected_output);
 }
 
 #[test]
 fn unsorted_list() {
     let input = vec![1, 5, 2];
-    let mut expected_output = vec![1, 2, 5];
-    let mut actual_output = unique(input);
-    expected_output.sort_unstable();
-    actual_output.sort_unstable();
+    let expected_output = vec![1, 2, 5];
+    let actual_output = unique(input);
     assert_eq!(actual_output, expected_output);
 }
 
 #[test]
 fn unsorted_list_with_duplicates() {
     let input = vec![1, 5, 2, 2, 1];
-    let mut expected_output = vec![1, 2, 5];
-    let mut actual_output = unique(input);
-    expected_output.sort_unstable();
-    actual_output.sort_unstable();
+    let expected_output = vec![1, 2, 5];
+    let actual_output = unique(input);
     assert_eq!(actual_output, expected_output);
 }
 
@@ -65,9 +70,7 @@ fn unsorted_list_with_duplicates() {
 fn sorted_list_with_duplicates() {
     let mut input = vec![1, 5, 2, 2, 1];
     input.sort_by(|x, y| x.partial_cmp(y).unwrap());
-    let mut expected_output = vec![1, 2, 5];
-    let mut actual_output = unique(input);
-    expected_output.sort_unstable();
-    actual_output.sort_unstable();
+    let expected_output = vec![1, 2, 5];
+    let actual_output = unique(input);
     assert_eq!(actual_output, expected_output);
 }
