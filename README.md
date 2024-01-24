c5bba022d3c756fdd2b8346b5d0e812a7465e136
Julio Valsesia <jcvalsesia@avaya.com>
2024-01-24 12:01:23 -0300

    Case-insensitive sort - Begin

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-# level-up-rust
-Rust advanced 
+# Level Up Rust: Challenge 4
+
+Your challenge is to implement a function, `sort_usernames()`, that takes a
+reference to vector of strings (`&Vec<String>`) as its input argument and sorts
+them in-place.
+
+## Testing your code
+
+To test your solution, use `cargo test`.
+
+```console
+$ cargo test
+...
+```
 
 # References
 https://github.com/LinkedInLearning/level-up-rust-3041143
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+fn sort_usernames<T: AsRef<str>>(usernames: &mut Vec<T>) {
+    todo!();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut users = vec!["Todd", "Amy", "mike99", "Jennifer", "alison"];
+
+    println!("unsorted: {:?}", &users);
+    sort_usernames(&mut users);
+    println!("sorted:   {:?}", &users);
+}
+
+#[test]
+fn five_users() {
+    let mut users = vec!["Todd", "Amy", "mike99", "Jennifer", "alison"];
+    let sorted = vec!["alison", "Amy", "Jennifer", "mike99", "Todd"];
+    sort_usernames(&mut users);
+
+    assert_eq!(users, sorted);
 }
