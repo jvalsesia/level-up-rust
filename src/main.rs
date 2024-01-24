a986f7a28359073c87e8b54e34d198e8827139f3
Julio Valsesia <jcvalsesia@avaya.com>
2024-01-24 14:25:04 -0300

    Convert between Celsius and Fahrenheit - End

--- src/main.rs
+++ src/main.rs
@@ -19,11 +19,17 @@ impl Temperature {
     }
 
     fn to_celsius(&self) -> f32 {
-        todo!();
+        match self.scale {
+            Scale::Celsius => self.degrees,
+            Scale::Fahrenheit => (self.degrees - 32.0) * (5.0 / 9.0),
+        }
     }
 
     fn to_fahrenheit(&self) -> f32 {
-        todo!();
+        match self.scale {
+            Scale::Celsius => ((9.0 / 5.0) * self.degrees) + 32.0,
+            Scale::Fahrenheit => self.degrees,
+        }
     }
 }
 
