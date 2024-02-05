921385ddc8d9c0ef6479555cfa2ccff242882bc8
Julio Valsesia <jcvalsesia@avaya.com>
2024-02-05 13:58:21 -0300

    Check If a file exists - Begin

--- README.md
+++ README.md
@@ -1,20 +1,19 @@
-# level-up-rust
-Rust advanced 
+# Level Up Rust: Challenge 12
 
-# Coding Challenges branches
-```sh
-git branch -vv
+Your challenge is to extend the `std::path::Path` type
+with an `is_writeable()` method.
+
+## Testing your solution
+
+Use `cargo test` to evaluate yourself:
+
+```console
+$ cargo test
+...
+running 2 tests
+test read_only ... ok
+test writeable ... ok
+...
 ```
-- **01_01:** Calculate the median
-- **01_02:** Find unique items
-- **01_03:** Print any text type
-- **01_04:** Case-insensitive sort
-- **01_05:** Convert text to Morse code
-- **01_06:** Value of a hand of cards
-- **01_07:** Has a deadline been reached?
-- **01_08:** Convert between Celsius and Fahrenheit
-- **01_09:** Sum a list of numbers with missing values
-- **01_10:10** Calculate the number of weeks between two dates
-
 # References
 https://github.com/LinkedInLearning/level-up-rust-3041143
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
+use std::path;
+
+trait FileMetadata {
+    fn exists(&self) -> bool;
+
+    fn is_writeable(&self) -> bool;
+
+    fn is_readable(&self) -> bool;
+}
+
+impl FileMetadata for path::Path {
+    fn is_readable(&self) -> bool {
+        todo!();
+    }
+
+    fn is_writeable(&self) -> bool {
+        todo!();
+    }
+
+    fn exists(&self) -> bool {
+        todo!();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // 
+}
+
+#[test]
+fn writeable() {
+    use std::fs;
+    use tempfile;
+
+    let f = tempfile::NamedTempFile::new().unwrap();
+    assert!(f.path().is_writeable());
+
+    fs::remove_file(f.path()).unwrap();
+}
+
+#[test]
+fn read_only() {
+    use std::fs;
+    use tempfile;
+
+    let f = tempfile::NamedTempFile::new().unwrap();
+    let mut perms = fs::metadata(f.path()).unwrap().permissions();
+    perms.set_readonly(true);
+    fs::set_permissions(f.path(), perms).unwrap();
+    assert_eq!(f.path().is_writeable(), false);
+
+    fs::remove_file(f.path()).unwrap();
 }
