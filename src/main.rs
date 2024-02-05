019819bf731321a523224dcc1d0d98edca971cee
Julio Valsesia <jcvalsesia@avaya.com>
2024-02-05 14:21:03 -0300

    Final challenge: Vigenere cipher

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+mod vigenere {
+    pub fn encrypt(plaintext: &str, key: &str) -> String {
+        String::new()
+    }
+
+    pub fn decrypt(ciphertext: &str, key: &str) -> String {
+        todo!()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let key = "WHYRUST";
+    let ciphertext = "
+    PVCDJG
+    PAYCMY
+    JR KUC
+    ";
+    let plaintext = vigenere::decrypt(&ciphertext, key);
+
+    println!("{}", plaintext);
 }
